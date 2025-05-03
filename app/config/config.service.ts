58934eb3de644039da8facad83e64f4905159918
Sergey Klimov <vomilk24@gmail.com>
2025-05-03 15:30:46 +0700

    feat: вынес тексты ошибок env в константы

--- app/config/config.service.ts
+++ app/config/config.service.ts
@@ -2,6 +2,12 @@ import { config, DotenvParseOutput } from "dotenv"
 
 import { IConfigService } from "./config.interface"
 
+import {
+  ENV_EMPTY,
+  ENV_NOT_FOUND,
+  ENV_KEY_NOT_EXIST,
+} from "../constants/env.constants"
+
 export class ConfigService implements IConfigService {
   private config: DotenvParseOutput
 
@@ -9,11 +15,11 @@ export class ConfigService implements IConfigService {
     const { error, parsed } = config()
 
     if (error) {
-      throw new Error("Файл .env не найден")
+      throw new Error(ENV_NOT_FOUND)
     }
 
     if (!parsed) {
-      throw new Error("Файл .env пуст")
+      throw new Error(ENV_EMPTY)
     }
 
     this.config = parsed
@@ -23,7 +29,7 @@ export class ConfigService implements IConfigService {
     const response = this.config[key]
 
     if (!response) {
-      throw new Error("Данный ключ отсутствует")
+      throw new Error(ENV_KEY_NOT_EXIST)
     }
 
     return response
---
+++ app/constants/env.constants.ts
@@ -0,0 +1,3 @@
+export const ENV_NOT_FOUND = "Файл .env не найден"
+export const ENV_EMPTY = "Файл .env пуст"
+export const ENV_KEY_NOT_EXIST = "Данный ключ отсутствует"
