0caf0fc4044cc7efa181feea1a278d6ad8f277e6
Sergey Klimov <vomilk24@gmail.com>
2025-05-04 16:33:05 +0700

    feat: добавил команду operation

--- app/buttons/commands/commands.buttons.ts
+++ app/buttons/commands/commands.buttons.ts
@@ -5,15 +5,21 @@ import { ICommands } from "./commands.interface"
 import {
   HELP_COMMAND_TEXT,
   REPORT_COMMAND_TEXT,
+  OPERATION_COMMAND_TEXT,
   CATEGORIES_COMMAND_TEXT,
   HELP_COMMAND_DESCRIPTION,
   REPORT_COMMAND_DESCRIPTION,
+  OPERATION_COMMAND_DESCRIPTION,
   CATEGORIES_COMMAND_DESCRIPTION,
 } from "../../constants/commands.constants"
 
 export class CommandsButtons implements ICommands {
   get(): readonly BotCommand[] {
     return [
+      {
+        command: OPERATION_COMMAND_TEXT,
+        description: OPERATION_COMMAND_DESCRIPTION,
+      },
       { command: REPORT_COMMAND_TEXT, description: REPORT_COMMAND_DESCRIPTION },
       {
         command: CATEGORIES_COMMAND_TEXT,
--- app/constants/commands.constants.ts
+++ app/constants/commands.constants.ts
@@ -2,8 +2,10 @@
 export const HELP_COMMAND_TEXT = "help"
 export const CATEGORIES_COMMAND_TEXT = "categories"
 export const REPORT_COMMAND_TEXT = "report"
+export const OPERATION_COMMAND_TEXT = "operation"
 
 // Descriptions
 export const HELP_COMMAND_DESCRIPTION = "помощь"
 export const CATEGORIES_COMMAND_DESCRIPTION = "список категорий"
 export const REPORT_COMMAND_DESCRIPTION = "вывести отчет"
+export const OPERATION_COMMAND_DESCRIPTION = "записать транзакцию"
