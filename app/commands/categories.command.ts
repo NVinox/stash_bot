7e5882b06e6ebfffd3acea3d86988c87d1ee678f
Sergey Klimov <vomilk24@gmail.com>
2025-06-01 14:43:19 +0700

    feat: добавил обработчики для новых команд категорий

--- app/commands/categories.command.ts
+++ app/commands/categories.command.ts
@@ -1,9 +1,14 @@
 import { Telegraf } from "telegraf"
 import { Command } from "./command.class"
 import { IBotContext } from "../context/context.interface"
-import { CATEGORIES_COMMAND_TEXT } from "../constants/commands.constants"
 import { ErrorHelper } from "../helpers/errors.helper"
 import { CategoriesMessage } from "../messages/commands/categories.message"
+import {
+  ADD_CATEGORIES_COMMAND_TEXT,
+  CATEGORIES_COMMAND_TEXT,
+  DELETE_CATEGORIES_COMMAND_TEXT,
+  EDIT_CATEGORIES_COMMAND_TEXT,
+} from "../constants/commands.constants"
 
 export class CategoriesCommand extends Command {
   constructor(bot: Telegraf<IBotContext>) {
@@ -11,14 +16,44 @@ export class CategoriesCommand extends Command {
   }
 
   handle(): void {
-    this.bot.command(CATEGORIES_COMMAND_TEXT, this.clickCommand)
+    this.bot.command(CATEGORIES_COMMAND_TEXT, this.sendCategories)
+    this.bot.command(ADD_CATEGORIES_COMMAND_TEXT, this.createCategory)
+    this.bot.command(EDIT_CATEGORIES_COMMAND_TEXT, this.editCategory)
+    this.bot.command(DELETE_CATEGORIES_COMMAND_TEXT, this.deleteCategory)
   }
 
-  private async clickCommand(ctx: IBotContext) {
+  private async sendCategories(ctx: IBotContext) {
     try {
       return await ctx.reply(new CategoriesMessage().getIntroductionHTML(), {
         parse_mode: "HTML",
       })
+    } catch (error) {
+      console.log(error)
+      await new ErrorHelper().sendInternalError(ctx)
+    }
+  }
+
+  private async createCategory(ctx: IBotContext) {
+    try {
+      return await ctx.reply("Добавление категории", { parse_mode: "HTML" })
+    } catch (error) {
+      console.log(error)
+      await new ErrorHelper().sendInternalError(ctx)
+    }
+  }
+
+  private async editCategory(ctx: IBotContext) {
+    try {
+      return await ctx.reply("Редактирование категории", { parse_mode: "HTML" })
+    } catch (error) {
+      console.log(error)
+      await new ErrorHelper().sendInternalError(ctx)
+    }
+  }
+
+  private async deleteCategory(ctx: IBotContext) {
+    try {
+      return await ctx.reply("Удаление категории", { parse_mode: "HTML" })
     } catch (error) {
       console.log(error)
       await new ErrorHelper().sendInternalError(ctx)
