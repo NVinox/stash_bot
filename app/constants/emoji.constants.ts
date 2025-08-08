3ace0d628a3dcbed249c7d4ef48add356c49bbff
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2025-08-09 02:10:49 +0700

    feat: добавил иконки к кнопкам

---
+++ app/constants/emoji.constants.ts
@@ -0,0 +1,8 @@
+export const EMOJI_TEXT = String.fromCodePoint(0x1f4c3)
+export const EMOJI_DOCUMENT = String.fromCodePoint(0x1f4d1)
+export const EMOJI_STOP = String.fromCodePoint(0x1f6ab)
+export const EMOJI_PEN = String.fromCodePoint(0x1f58a)
+export const EMOJI_PLUS = "\u2795"
+
+export const EMOJI_GRAPH_UP = String.fromCodePoint(0x1f4c8)
+export const EMOJI_GRAPH_DOWN = String.fromCodePoint(0x1f4c9)
--- app/constants/keyboards.constants.ts
+++ app/constants/keyboards.constants.ts
@@ -1,9 +1,19 @@
-export const MAKE_EXPENSE = "Внести расход"
-export const MAKE_INCOME = "Внести доход"
+import {
+  EMOJI_DOCUMENT,
+  EMOJI_GRAPH_DOWN,
+  EMOJI_GRAPH_UP,
+  EMOJI_PEN,
+  EMOJI_PLUS,
+  EMOJI_STOP,
+  EMOJI_TEXT,
+} from "./emoji.constants"
 
-export const TEXT_FORMAT = "Текстовый"
-export const DOCUMENT_FORMAT = "Документ"
+export const MAKE_EXPENSE = `Внести расход ${EMOJI_GRAPH_UP}`
+export const MAKE_INCOME = `Внести доход ${EMOJI_GRAPH_DOWN}`
 
-export const EDIT_CATEGORY = "Редактировать"
-export const DELETE_CATEGORY = "Удалить"
-export const ADD_CATEGORY = "Добавить"
+export const TEXT_FORMAT = `Текстовый ${EMOJI_TEXT}`
+export const DOCUMENT_FORMAT = `Документ ${EMOJI_DOCUMENT}`
+
+export const EDIT_CATEGORY = `Редактировать ${EMOJI_PEN}`
+export const DELETE_CATEGORY = `Удалить ${EMOJI_STOP}`
+export const ADD_CATEGORY = `Добавить ${EMOJI_PLUS}`
