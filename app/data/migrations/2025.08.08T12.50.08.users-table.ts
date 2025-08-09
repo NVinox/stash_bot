b00b5d3cb9e7435a5c9fb6f0c055ceccb17c9fdc
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2025-08-09 15:16:59 +0700

    fix: поправил миграции

--- app/data/migrations/2025.08.08T12.50.08.users-table.ts
+++ app/data/migrations/2025.08.08T12.50.08.users-table.ts
@@ -2,33 +2,41 @@ import { Sequelize, DataType } from "sequelize-typescript"
 import type { MigrationFn } from "umzug"
 
 export const up: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().createTable("users", {
-    id: {
-      type: DataType.BIGINT,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    telegramId: {
-      type: DataType.BIGINT,
-      unique: true,
-      allowNull: false,
-    },
-    name: {
-      type: DataType.STRING,
-      allowNull: false,
-    },
-    createdAt: {
-      type: DataType.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-    },
-    updatedAt: {
-      type: DataType.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-    },
-  })
+  const transaction = await sequelize.transaction()
+
+  try {
+    await sequelize.getQueryInterface().createTable("users", {
+      id: {
+        type: DataType.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      telegramId: {
+        type: DataType.BIGINT,
+        unique: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataType.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataType.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        type: DataType.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    })
+
+    await transaction.commit()
+  } catch (error) {
+    console.log(error)
+    await transaction.rollback()
+  }
 }
 export const down: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
   await sequelize.getQueryInterface().dropTable("users")
--- app/data/migrations/2025.08.08T19.39.30.categories_expense_table.ts
+++ app/data/migrations/2025.08.08T19.39.30.categories_expense_table.ts
@@ -2,35 +2,44 @@ import { Sequelize, DataType } from "sequelize-typescript"
 import type { MigrationFn } from "umzug"
 
 export const up: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().createTable("categories_expense", {
-    id: {
-      type: DataType.BIGINT,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    userId: {
-      type: DataType.BIGINT,
-      allowNull: false,
-      references: {
-        model: "users",
-        key: "id",
+  const transaction = await sequelize.transaction()
+
+  try {
+    await sequelize.getQueryInterface().createTable("categories_expense", {
+      id: {
+        type: DataType.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
       },
-    },
-    title: {
-      type: DataType.STRING,
-      allowNull: false,
-    },
-    createdAt: {
-      type: DataType.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-    },
-    updatedAt: {
-      type: DataType.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-    },
-  })
+      userId: {
+        type: DataType.BIGINT,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      title: {
+        type: DataType.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataType.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        type: DataType.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    })
+
+    await transaction.commit()
+  } catch (error) {
+    console.log(error)
+    await transaction.rollback()
+  }
 }
 
 export const down: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
